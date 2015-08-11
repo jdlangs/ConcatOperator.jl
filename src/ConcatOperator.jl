8563b98076db6ee976e6069fbcda45db28f3065e
Josh Langsfeld <jdlangs@gmail.com>
2015-08-11 09:14:24 -0400

    First methods and tests

--- src/ConcatOperator.jl
+++ src/ConcatOperator.jl
@@ -1,5 +1,17 @@
 module ConcatOperator
 
-# package code goes here
+VERSION < v"0.4.0-dev+6294" && error("""
+    Cannot load ConcatOperator module.
+    This version of Julia does not support parsing "++"
+    """)
 
-end # module
+export ++
+
+"""
+Concatenation operator for strings, characters, and vectors.
+"x ++ y ++ z ++ ..." is transformed by the parser to "++(x,y,z,...)".
+"""
+++(ss::Union{AbstractString,Char}...) = string(ss...)
+++(x1::Vector, xs::Vector...) = vcat(x1, xs...)
+
+end
